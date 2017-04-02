9331c01daf8b629f7ca05373bdb8472f61727ffc
Michael Jeffrey Wu <michaeljeffreywu@gmail.com>
2017-04-02 01:06:52 -0700

    :bento: new assets

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -16,8 +16,8 @@ export default class Intro extends Component {
         <Onboarding
           pages={[
             { backgroundColor: '#78c9f2', image: <Image source={require('../assets/animated_map.gif')} style={{width: 300, height: 300}}/>, title: 'Navigate', subtitle: 'Walking Directions Reinvented' },
-            { backgroundColor: "#78c9f2", image: <Image source={require('../assets/map.png')} style={{width: 250, height: 200}}/>, title: 'Direct', subtitle: 'Enter a location to walk to' },
-            { backgroundColor: "#fff", image: <Image source={require('../assets/AR.png')} style={{width: 125, height: 200}}/>, title: 'View', subtitle: 'View directions in Augmented Reality' },
+            { backgroundColor: "#a6e0a7", image: <Image source={require('../assets/map.png')} style={{width: 250, height: 200}}/>, title: 'Direct', subtitle: 'Enter a location to walk to' },
+            { backgroundColor: "#f6f6ee", image: <Image source={require('../assets/AR.png')} style={{width: 200, height: 200}}/>, title: 'View', subtitle: 'View directions in Augmented Reality' },
           ]}
           onEnd={() => this.setState({ completedOnboarding: true })}
         />
